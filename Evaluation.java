32bddd46053ae936f4c562ce5624e842931030a2
Katherine Gershfeld <ka.gershfeld@gmail.com>
2015-01-09 01:21:30 -0500

    evaluation is now also recursive and does exponents

--- Evaluation.java
+++ Evaluation.java
@@ -1,12 +1,14 @@
 import java.util.*;
-import java.lang.Math;
-public class Evaluation{
+import java.lang.*;
 
-    //separates expression into parentheses
+public class Evaluation{
+    //recursively separates expression into parentheses
     public static double evaluateParens(String expression){
 	int startParen = 0;
 	int endParen = 0;
 	for(int i=0; i<expression.length(); i++){
+	    //System.out.println("llo");
+	    //System.out.println(expression);
 	    if(expression.charAt(i)=='('){
 		startParen++;
 	    }else if(expression.charAt(i)==')'){
@@ -14,29 +16,20 @@ public class Evaluation{
 	    }
 	    if(startParen==endParen && startParen!=0){
 		int start = expression.indexOf('(');
-		System.out.println("evaluateParens("+expression.substring(0,start)+")+evaluateParens("+expression.substring(start,i)+")'"+expression.substring(i+1)+"')");
-		System.out.println("evaluateParens("+expression.substring(0,start)+")+evaluateParens("+expression.substring(start+1,i)+")'"+expression.substring(i+1)+"')");
-		//if(expression.charAt(i)==')'){
-		//return evaluateParens(expression.substring(0,start)+evaluateParens(expression.substring(start+1,i)));
-		//}else{
+		//System.out.println("evaluateParens("+expression.substring(0,start)+")+evaluateParens("+expression.substring(start,i)+")'"+expression.substring(i+1)+"')");
+		//System.out.println("evaluateParens("+expression.substring(0,start)+")+evaluateParens("+expression.substring(start+1,i)+")'"+expression.substring(i+1)+"')");
 		return evaluateParens(expression.substring(0,start)+evaluateParens(expression.substring(start+1,i))+expression.substring(i+1));
-		    //}
 	    }
 	}
+	//System.out.println(expression);
 	return evaluateMath(expression);
     }
-
-    public static double evaluateMath(String expression){
+    //outdated expression evaluator
+    public static double eval(String expression){
 	String[] mult,frac;
 	ArrayList<String> operators = new ArrayList<String>();
 	ArrayList<Double> operands = new ArrayList<Double>();
 	expression=expression.replace("-","+-");
-	for (int i=0;i<expression.length();i++){
-	    String here = expression.substring(i,i+1);
-	    if (here.equals("+")){
-		operators.add(here);
-	    }	    
-	}
 	expression=expression.replace("/","*1/");
 	String[] a = expression.split("[+]");
 	for (int i=0; i<a.length; i++){
@@ -65,10 +58,10 @@ public class Evaluation{
 	double out=operands.get(0);
 	System.out.println(operators);
 	for (int i=0;i<operands.size()-1;i++){
-	    System.out.println(out);
+	    /*System.out.println(out);
 	    System.out.println(operands);
 	    System.out.println(operators.get(i));
-	    //if (operators.get(i).equals("+")){
+	    //if (operators.get(i).equals("+")){*/
 		out+=operands.get(i+1);
 		//else{
 		//out-=operands.get(i+1);
@@ -77,9 +70,57 @@ public class Evaluation{
 	}
 	return out;
     }
-    public static double evaluateMath(ArrayList<String> in){
-	return evaluateMath(combine(in));
+    //recursive expression evaluation
+    public static double evaluateMath(String expression){
+	expression=expression.replace("-","+-");
+	expression=expression.replace("/","*1/");
+	return Double.parseDouble(evaluateHelper(expression,0));
+    }
+    //helper
+    public static String evaluateHelper(String expression, int delimiter){
+	String[] pemdas = {"+","*","/","\\^"};
+	String[] oper = expression.split("["+pemdas[delimiter]+"]");
+	for (int i=0; i<oper.length; i++){
+	    if (delimiter<=2){
+		oper[i]=evaluateHelper(oper[i],delimiter+1);
+	    }
+	}
+	int k = 0;
+	double out = 0.0;
+	if (oper.length>0){
+	    while (k<oper.length && oper[k].length()==0){
+		k++;
+	    }
+	    if (k<oper.length){
+		out = Double.parseDouble(oper[k]);
+		for (int i=k; i<oper.length-1; i++){
+		    if (oper[k].length()>0){
+			switch (delimiter){
+			case 0: 
+			    out+=Double.parseDouble(oper[i+1]);
+			    break;
+			case 1: 
+			    out*=Double.parseDouble(oper[i+1]);
+			    break;
+			case 2: 
+			    out/=Double.parseDouble(oper[i+1]);
+			    break;
+			case 3: 
+			    out=Math.pow(out,Double.parseDouble(oper[i+1]));
+			    break;
+			}
+		    }
+		}
+	    }
+	    return ""+out;
+	}
+	return "";
+    }
+    //overloading of parentheses evaluation for String ArrayLists 
+    public static double evaluateParens(ArrayList<String> in){
+	return evaluateParens(combine(in));
     } 
+    //adds all elements of a String ArrayList into one String
     public static String combine(ArrayList<String> in){
 	String joined = "";
 	for (int i=0; i<in.size(); i++){
@@ -100,8 +141,8 @@ public class Evaluation{
 	a.add("6");
 	a.add("*");
 	a.add("4");
-	System.out.println("0: "+evaluateMath("4-5*5+7/6.0*4"));
-	System.out.println("0: "+evaluateMath(a));
+	System.out.println("0: "+evaluateParens("4-5*5+7/6.0*4"));
+	System.out.println("0: "+evaluateParens(a));
 	System.out.println("1: "+evaluateParens("(2+3)*(7)"));
 	System.out.println("2: "+evaluateParens("7*(2+3)"));
 	System.out.println("3: "+evaluateParens("((4+8)*3)+((5-6)*1)*1"));
